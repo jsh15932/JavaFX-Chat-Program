e0a4fa1b8538afd5797debb8092b2b8ec5613763
Hoon <jsh1400512@naver.com>
2019-03-11 13:49:11 +0900

    Create Project

--- Chat Server/src/application/Client.java
+++ Chat Server/src/application/Client.java
@@ -1,6 +1,10 @@
 package application;
 
+import java.io.IOException;
 import java.net.Socket;
+
+import org.omg.CORBA.portable.InputStream;
+import org.omg.CORBA.portable.OutputStream;
 
 public class Client {
 
@@ -12,10 +16,63 @@ public class Client {
 	}
 	
 	public void receive() {
-		
+		Runnable thread = new Runnable() {
+			@Override
+			public void run() {
+				try {
+					while(true) {
+						InputStream in = (InputStream) socket.getInputStream();
+						byte[] buffer = new byte[512];
+						
+						int length = in.read(buffer);
+						if(length == -1) throw new IOException();
+						System.out.println("[메시지 수신 성공]"
+								+ socket.getRemoteSocketAddress()
+								+ ": " + Thread.currentThread().getName());
+						
+						String message = new String(buffer, 0, length, "UTF-8");
+						for(Client client : Main.clients) {
+							client.send(message);
+						}
+					}
+				} catch (Exception e) {
+					try {
+						System.out.println("[메시지 수신 오류]"
+								+ socket.getRemoteSocketAddress()
+								+ ": " + Thread.currentThread().getName());
+						Main.clients.remove(Client.this);
+						socket.close();
+					} catch (Exception e2) {
+						e2.printStackTrace();
+					}
+				}
+			}
+		};
+		Main.threadPool.submit(thread);
 	}
 	
 	public void send(String message) {
-		
+		Runnable thread = new Runnable() {
+			@Override
+			public void run() {
+				try {
+					OutputStream out = (OutputStream) socket.getOutputStream();
+					byte[] buffer = message.getBytes("UTF-8");
+					out.write(buffer);
+					out.flush();
+				} catch(Exception e) {
+					try {
+						System.out.println("[메시지 송신 오류]"
+								+ socket.getRemoteSocketAddress()
+								+ ": " + Thread.currentThread().getName());
+						Main.clients.remove(Client.this);
+						socket.close();
+					} catch (Exception e2) {
+						e2.printStackTrace();
+					}
+				}
+			}
+		};
+		Main.threadPool.submit(thread);
 	}
 }
--- Chat Server/src/application/Main.java
+++ Chat Server/src/application/Main.java
@@ -1,24 +1,26 @@
 package application;
-	
+
+import java.net.ServerSocket;
+import java.util.Vector;
+import java.util.concurrent.ExecutorService;
+
 import javafx.application.Application;
-
 import javafx.stage.Stage;
-import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
-
 
 public class Main extends Application {
+
+	public static ExecutorService threadPool;
+	public static Vector<Client> clients = new Vector<Client>();
+	
+	ServerSocket serverSocket;
+	
+	public void startServer(String IP, int port) { }
+	
+	public void stopServer() { }
+	
 	@Override
 	public void start(Stage primaryStage) {
-		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
+
 	}
 	
 	public static void main(String[] args) {
