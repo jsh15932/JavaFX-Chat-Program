a823b5c516a853bc248507ee09d0499389089ab0
Hoon <jsh1400512@naver.com>
2019-03-12 12:48:39 +0900

    Create Project

--- Chat Server/src/application/Client.java
+++ Chat Server/src/application/Client.java
@@ -1,7 +1,12 @@
 package application;
 
 import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Iterator;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
 
 import org.omg.CORBA.portable.InputStream;
 import org.omg.CORBA.portable.OutputStream;
@@ -9,6 +14,8 @@ import org.omg.CORBA.portable.OutputStream;
 public class Client {
 
 	Socket socket;
+	private ServerSocket serverSocket;
+	private ThreadPoolExecutor threadPool;
 	
 	public Client(Socket socket) {
 		this.socket = socket;
@@ -74,5 +81,55 @@ public class Client {
 			}
 		};
 		Main.threadPool.submit(thread);
+	}
+	
+	public void startServer(String IP, int port) {
+		try {
+			serverSocket = new ServerSocket();
+			serverSocket.bind(new InetSocketAddress(IP, port));
+		} catch (Exception e) {
+			e.printStackTrace();
+			if(!serverSocket.isClosed())
+				stopServer();
+			return;
+		}
+		
+		Runnable thread = new Runnable() {
+			@Override
+			public void run() {
+				while(true) {
+					try {
+						Socket socket = serverSocket.accept();
+						clients.add(new Client(socket));
+						System.out.println("[클라이언트 접속] "
+								+ socket.getRemoteSocketAddress()
+								+ ": " + Thread.currentThread().getName());
+					} catch (Exception e) {
+						if(!serverSocket.isClosed())
+							stopServer();
+						break;
+					}
+				}
+			}
+		};
+		threadPool = Executors.newCachedThreadPool();
+		threadPool.submit(thread);
+	}
+
+	private void stopServer() {
+		try {
+			Iterator<Client> iterator = clients.iterator();
+			while(iterator.hasNext()) {
+				Client client = iterator.next();
+				client.socket.close();
+				iterator.remove();
+			}
+			if(serverSocket != null && !serverSocket.isClosed())
+				serverSocket.close();
+			if(threadPool != null && !threadPool.isShutdown())
+				threadPool.shutdown();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
--- Chat Server/src/application/Main.java
+++ Chat Server/src/application/Main.java
@@ -20,7 +20,7 @@ public class Main extends Application {
 	
 	@Override
 	public void start(Stage primaryStage) {
-
+		
 	}
 	
 	public static void main(String[] args) {
