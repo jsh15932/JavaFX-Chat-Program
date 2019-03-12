03e9acb9e5f059bf6011fcf3a1f6d32c30cc95ac
Hoon <jsh1400512@naver.com>
2019-03-12 13:46:42 +0900

    Create Project

--- Chat Server/src/application/Client.java
+++ Chat Server/src/application/Client.java
@@ -1,21 +1,31 @@
 package application;
 
+import java.awt.Insets;
+import java.awt.TextArea;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Iterator;
+import java.util.Vector;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadPoolExecutor;
 
 import org.omg.CORBA.portable.InputStream;
 import org.omg.CORBA.portable.OutputStream;
 
+import javafx.application.Platform;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
+
 public class Client {
 
 	Socket socket;
 	private ServerSocket serverSocket;
 	private ThreadPoolExecutor threadPool;
+	private Vector<Client> clients;
 	
 	public Client(Socket socket) {
 		this.socket = socket;
@@ -82,54 +92,6 @@ public class Client {
 		};
 		Main.threadPool.submit(thread);
 	}
-	
-	public void startServer(String IP, int port) {
-		try {
-			serverSocket = new ServerSocket();
-			serverSocket.bind(new InetSocketAddress(IP, port));
-		} catch (Exception e) {
-			e.printStackTrace();
-			if(!serverSocket.isClosed())
-				stopServer();
-			return;
-		}
-		
-		Runnable thread = new Runnable() {
-			@Override
-			public void run() {
-				while(true) {
-					try {
-						Socket socket = serverSocket.accept();
-						clients.add(new Client(socket));
-						System.out.println("[클라이언트 접속] "
-								+ socket.getRemoteSocketAddress()
-								+ ": " + Thread.currentThread().getName());
-					} catch (Exception e) {
-						if(!serverSocket.isClosed())
-							stopServer();
-						break;
-					}
-				}
-			}
-		};
-		threadPool = Executors.newCachedThreadPool();
-		threadPool.submit(thread);
-	}
-
-	private void stopServer() {
-		try {
-			Iterator<Client> iterator = clients.iterator();
-			while(iterator.hasNext()) {
-				Client client = iterator.next();
-				client.socket.close();
-				iterator.remove();
-			}
-			if(serverSocket != null && !serverSocket.isClosed())
-				serverSocket.close();
-			if(threadPool != null && !threadPool.isShutdown())
-				threadPool.shutdown();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
 }
+
+	
